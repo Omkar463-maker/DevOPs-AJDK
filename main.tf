5c0cbf850ac0af7996aa55ef2fd176edd62953ab
Omkar463-maker <124853573+Omkar463-maker@users.noreply.github.com>
2023-05-21 17:13:22 -0700

    Updated

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web"{
 
     count         =  3
     ami           =  "ami-03f65b8614a860c29"
-    instance_type = "t3.micro"
+    instance_type = "t2.micro"
 
     tags = {
         Name = "Jenkins"
